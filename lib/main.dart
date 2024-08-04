3a703bf6efa3658dfc50cad53d84b67c92ad4660
Junn Eric Timoteo <juntyms@yahoo.com>
2024-08-04 19:35:51 +0400

    character factory

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,16 @@ import 'package:inventory_app/my_home_page.dart';
 import 'package:inventory_app/product_store.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
--- lib/product_model.dart
+++ lib/product_model.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Product {
   Product({
     required this.id,
@@ -14,4 +16,33 @@ class Product {
   int qty;
   String image;
   bool isFav = false;
+
+//Create a map
+// product to firestore
+  Map<String, dynamic> toFirestore() {
+    return {
+      'categoryid': categoryId,
+      'name': name,
+      'qty': qty,
+      'image': image,
+      'isFav': isFav,
+    };
+  }
+
+// from firestore
+  factory Product.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options) {
+    final data = snapshot.data()!;
+
+    // get data from snapshot
+    Product product = Product(
+        id: snapshot.id,
+        categoryId: data['categoryId'],
+        name: data['name'],
+        qty: data['qty'],
+        image: data['image'],
+        isFav: data['isFav']);
+
+    return product;
+  }
 }
